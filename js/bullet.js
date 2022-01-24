50a897921195fcb82c9ade41a2d35e00ed8176d6
KGurchiek <90143063+KGurchiek@users.noreply.github.com>
2022-01-24 10:16:11 -0800

    Update bullet.js

--- js/bullet.js
+++ js/bullet.js
@@ -1456,12 +1456,7 @@ const b = {
                 
                 m.maxEnergy = 100
                 
-                if (m.immuneCycle < m.cycle + 60) m.immuneCycle = m.cycle + tech.collisionImmuneCycles; //player is immune to damage for 30 cycles
-                b.harpoon(where, closest.target, m.angle, harpoonSize, false, 15)
-                m.fireCDcycle = m.cycle + 50 * b.fireCDscale; // cool down
-                const speed = 50
-                const velocity = { x: speed * Math.cos(m.angle), y: speed * Math.sin(m.angle) }
-                Matter.Body.setVelocity(player, velocity);
+                
                 
                 if (Vector.magnitude(Vector.sub(this.position, m.pos)) < returnRadius) { //near player
                     this.endCycle = 0;
