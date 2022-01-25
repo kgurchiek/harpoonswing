f0d29a144447a4994d9a735e77333ed639b3cb9f
KGurchiek <90143063+KGurchiek@users.noreply.github.com>
2022-01-25 14:02:34 -0800

    Update bullet.js

--- js/bullet.js
+++ js/bullet.js
@@ -5816,7 +5816,7 @@ const b = {
                         }
                     } else {
                         
-                         //grappling hook
+                         //lunge with thrown harpoon (when crouched)
                          if (true) {
                              if (m.immuneCycle < m.cycle + 60) m.immuneCycle = m.cycle + tech.collisionImmuneCycles; //player is immune to damage for 30 cycles
                              b.harpoon(where, closest.target, m.angle, harpoonSize, false, 15)
